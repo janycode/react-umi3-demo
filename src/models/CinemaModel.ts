f113bf9c2b4755382e42ea32e9863314a9aad4ef
姜源 <yuan62387@qq.com>
2026-01-29 11:00:23 +0800

    react umi 集成dva的异步数据流，传递参数请求新的数据，以及loading.global关联加载中组件

--- src/models/CinemaModel.ts
+++ src/models/CinemaModel.ts
@@ -2,22 +2,28 @@ export default {
   namespace: 'cinema',
   state: {
     list: [],
+  },
+  reducers: {
+    changeList(prevState: any, action: any) {
+      console.log('cinema action=', action);
+      return {
+        ...prevState,
+        list: action.payload,
+      };
     },
-    reducers: {
-        changeList(prevState: any, action: any) {
-            console.log('cinema action=', action);
-            return {
-                ...prevState,
-                list: action.payload
-            }
-      }
+    clearList(prevState: any, action: any) {
+      return {
+        ...prevState,
+        list: [],
+      };
+    },
   },
   effects: {
     // 【异步数据流】要放在 effects 中使用生成器函数，才能被 dispatch
     *getList(action: any, { call, put }: any): any {
-      console.log('getList');
-      // call 调用 异步请求方法
-      var res = yield call(requestCinemaList);
+      console.log('getList', action);
+      // call 调用 异步请求方法，携带参数
+      var res = yield call(requestCinemaList, action.payload.cityId);
       yield put({
         type: 'changeList', //进入 同步数据流 reducers
         payload: res,
@@ -26,16 +32,16 @@ export default {
   },
 };
 
-async function requestCinemaList() {
-    var res = await fetch(
-        `https://m.maizuo.com/gateway?cityId=410100&ticketFlag=1&k=8862890`,
-        {
-            headers: {
-                'x-client-info':
-                    '{"a":"3000","ch":"1002","v":"5.2.1","e":"17689720181688867040133121","bc":"440300"}',
-                'x-host': 'mall.film-ticket.cinema.list',
-            },
-        },
-    ).then((res) => res.json());
-    return res.data.cinemas;
+async function requestCinemaList(cityId: string) {
+  var res = await fetch(
+    `https://m.maizuo.com/gateway?cityId=${cityId}&ticketFlag=1&k=8862890`,
+    {
+      headers: {
+        'x-client-info':
+          '{"a":"3000","ch":"1002","v":"5.2.1","e":"17689720181688867040133121","bc":"440300"}',
+        'x-host': 'mall.film-ticket.cinema.list',
+      },
+    },
+  ).then((res) => res.json());
+  return res.data.cinemas;
 }
--- src/pages/Cinema.tsx
+++ src/pages/Cinema.tsx
@@ -1,4 +1,4 @@
-import { NavBar, Toast } from 'antd-mobile';
+import { NavBar, DotLoading } from 'antd-mobile';
 import { DownOutline, SearchOutline } from 'antd-mobile-icons';
 import { useEffect } from 'react';
 import { connect, history } from 'umi';
@@ -8,7 +8,10 @@ function Cinema(props: any) {
     if (props.list.length === 0) {
       //请求数据
       props.dispatch({
-        type: "cinema/getList"
+        type: "cinema/getList",
+        payload: {
+          cityId: props.cityId  //带着参数
+        }
       })
     } else {
       console.log("cinema list 走缓存");
@@ -16,6 +19,10 @@ function Cinema(props: any) {
   }, []);
 
   const back = () => {
+    // 清空 cinema list，为了展示切换城市后最新的数据
+    props.dispatch({
+      type:"cinema/clearList"
+    })
     history.push('/city');
   };
   return (
@@ -33,12 +40,17 @@ function Cinema(props: any) {
       >
         影院
       </NavBar>
+      {
+        props.loading && 
+        /* 加载中 白点... */
+        <span style={{ fontSize: 14 }}>
+          <DotLoading />
+        </span>
+      }
       <ul>
-        {
-          props.list.map((item:any) => 
-            <li key={item.cinemaId}>{item.name}</li>
-          )
-        }
+        {props.list.map((item: any) => (
+          <li key={item.cinemaId}>{item.name}</li>
+        ))}
       </ul>
     </div>
   );
@@ -49,7 +61,9 @@ const MapStateToProps = (state: any) => {
   console.log(state);
   return {
     a: 1,
+    loading: state.loading.global,
     cityName: state.city.cityName,
+    cityId: state.city.cityId,
     list: state.cinema.list,
   };
 };
